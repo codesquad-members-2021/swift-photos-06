ab61d3fda4cdee510c2b9d61ee37f6d9b43e1c4c
JinseoPark <rarebook92@naver.com>
2021-03-22 17:58:39 +0900

    Create a CustomCollectionViewCell
    Merged with Lollo

---
+++ PhotoApps/PhotoApps/MyCustomViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCustomViewCell.swift
+//  PhotoApps
+//
+//  Created by jinseo park on 3/22/21.
+//
+
+import UIKit
+
+class MyCustomViewCell: UICollectionViewCell {
+//    @IBOutlet weak var label : UILabel!
+}
