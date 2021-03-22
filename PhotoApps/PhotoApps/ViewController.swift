2c5e943567c45bfdae9925676a7e1aa1e56324d4
Song Lee <songlish233@gmail.com>
2021-03-22 17:09:07 +0900

    Configure 40 random color cells to collectionView

--- PhotoApps/PhotoApps/ViewController.swift
+++ PhotoApps/PhotoApps/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private let numberOfItems = 40
+    private let cellSizeUnit: CGFloat = 80
+    private var randomColorCollection = [UIColor]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        randomColorCollection = generateRandomColors(count: numberOfItems)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
-
-
+    
+    private func generateRandomColors(count: Int) -> [UIColor] {
+        var colorCollection = [UIColor]()
+        
+        for _ in 0..<count {
+            let newColor = UIColor.init(red: CGFloat(drand48()),
+                                        green: CGFloat(drand48()),
+                                        blue: CGFloat(drand48()),
+                                        alpha: 1)
+            colorCollection.append(newColor)
+        }
+        return colorCollection
+    }
 }
 
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: cellSizeUnit, height: cellSizeUnit)
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfItems
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let newCell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath)
+        newCell.backgroundColor = randomColorCollection[indexPath.row]
+        return newCell
+    }
+}
