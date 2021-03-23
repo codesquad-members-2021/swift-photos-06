c7951d26ff70b1e12c87878af3f85eb9d83c2c0c
Song Lee <songlish233@gmail.com>
2021-03-23 19:55:26 +0900

    Refactoring, Delete unusable function

--- PhotoApps/PhotoApps/ViewController.swift
+++ PhotoApps/PhotoApps/ViewController.swift
@@ -11,80 +11,68 @@ import Photos
 class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
-        
-    private var numberOfItems = 40
-    private let cellSizeUnit: CGFloat = 100
-    private var randomColorCollection = [UIColor]()
-    private var allPhotoAssets : PHFetchResult<PHAsset>?
+    
+    private let cellSize = CGSize(width: 100, height: 100)
+    private var photoAssets : PHFetchResult<PHAsset>?
+    private var cellCount: Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        randomColorCollection = generateRandomColors(count: numberOfItems)
-        
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
         
-        let targetAlbums = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .any, options: .none)
-        
-        let album = targetAlbums.firstObject ?? PHAssetCollection()
-
-        self.allPhotoAssets = PHAsset.fetchAssets(in: album, options: .none)
-        self.numberOfItems = self.allPhotoAssets?.count ?? 0
+        configurePhotoAssets()
+        updateCellCount()
         
         registerPhotoLibrary()
-                
-        self.collectionView.reloadData()
-        
     }
-
-    private func generateRandomColors(count: Int) -> [UIColor] {
-        var colorCollection = [UIColor]()
-        
-        for _ in 0..<count {
-            let newColor = UIColor.init(red: CGFloat(drand48()),
-                                        green: CGFloat(drand48()),
-                                        blue: CGFloat(drand48()),
-                                        alpha: 1)
-            colorCollection.append(newColor)
-        }
-        return colorCollection
+    
+    private func configurePhotoAssets() {
+        let smartAlbumCollection = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .any, options: .none)
+        let allContents = smartAlbumCollection.firstObject ?? PHAssetCollection()
+        photoAssets = PHAsset.fetchAssets(in: allContents, options: .none)
+    }
+    
+    private func updateCellCount() {
+        cellCount = photoAssets?.count ?? 0
     }
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: cellSizeUnit, height: cellSizeUnit)
+        return cellSize
     }
 }
 
 extension ViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return numberOfItems
+        return cellCount
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let newCell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! MyCustomViewCell
         
-        if let asset = allPhotoAssets?[numberOfItems - indexPath.row - 1] {
+        let photoIdxToShow = cellCount - indexPath.row - 1 //최신 항목부터 표시
+        
+        if let asset = photoAssets?[photoIdxToShow] {
             newCell.photoCellImageView.image = getAssetThumbnail(asset: asset)
         }
         
         return newCell
     }
     
-    func getAssetThumbnail(asset: PHAsset) -> UIImage {
-        let manager =  PHCachingImageManager() //PHImageManager()
+    private func getAssetThumbnail(asset: PHAsset) -> UIImage {
+        let manager =  PHCachingImageManager()
         
         let option = PHImageRequestOptions()
         option.isSynchronous = true
         option.resizeMode = .exact
         
         var thumbnail = UIImage()
-        let targetSize = CGSize(width: cellSizeUnit, height: cellSizeUnit)
                 
-        manager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFill, options: option) { (result, _) in
+        manager.requestImage(for: asset, targetSize: cellSize, contentMode: .aspectFill, options: option) { (result, _) in
             if let result = result {
                 thumbnail = result
             }
@@ -92,7 +80,6 @@ extension ViewController: UICollectionViewDataSource {
         return thumbnail
     }
 }
-
 
 extension ViewController : PHPhotoLibraryChangeObserver {
     
@@ -102,13 +89,13 @@ extension ViewController : PHPhotoLibraryChangeObserver {
     
     func photoLibraryDidChange(_ changeInstance: PHChange) {
         
-        guard let asset = allPhotoAssets,
+        guard let asset = photoAssets,
               let changes = changeInstance.changeDetails(for: asset) else { return }
         
         OperationQueue.main.addOperation {
             
-            self.allPhotoAssets = changes.fetchResultAfterChanges
-            self.numberOfItems = self.allPhotoAssets?.count ?? 0
+            self.photoAssets = changes.fetchResultAfterChanges
+            self.updateCellCount()
         
             guard changes.hasIncrementalChanges else { //아주 큰 변화가 있을 시엔 전체를 reload해야 함
                 self.collectionView.reloadData()
