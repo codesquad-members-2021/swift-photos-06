6dd43217199776c0fd7add2dcc945765c7969702
JinseoPark <rarebook92@naver.com>
2021-03-23 12:31:36 +0900

    Add getImageThumbnails function,
    Get ImageThumbnails using PHImageManager.
    To Fix : Sizing Issue, showed random image order.

--- PhotoApps/PhotoApps/ViewController.swift
+++ PhotoApps/PhotoApps/ViewController.swift
@@ -6,20 +6,28 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
-    
-    private let numberOfItems = 40
+        
+    private var numberOfItems = 40
     private let cellSizeUnit: CGFloat = 100
     private var randomColorCollection = [UIColor]()
+    private var allPhotoAssets : PHFetchResult<PHAsset>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         randomColorCollection = generateRandomColors(count: numberOfItems)
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+        
+        self.allPhotoAssets = PHAsset.fetchAssets(with: .none)
+        self.numberOfItems = self.allPhotoAssets?.count ?? 0
+        
+        self.collectionView.reloadData()
+        
     }
     
     private func generateRandomColors(count: Int) -> [UIColor] {
@@ -48,9 +56,28 @@ extension ViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let newCell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! MyCustomViewCell
         
-        newCell.backgroundColor = randomColorCollection[indexPath.row]
+//        newCell.backgroundColor = randomColorCollection[indexPath.row]
+        
+        if let asset = allPhotoAssets?[numberOfItems - indexPath.row - 1] {
+            newCell.photoCellImageView.image = getAssetThumbnail(asset: asset)
+        }
+        
         return newCell
+    }
+    
+    func getAssetThumbnail(asset: PHAsset) -> UIImage {
+        let manager = PHImageManager()
+        let option = PHImageRequestOptions()
+        var thumbnail = UIImage()
+        option.isSynchronous = true
+        manager.requestImage(for: asset, targetSize: CGSize.init(width: cellSizeUnit, height: cellSizeUnit), contentMode: .aspectFit, options: .none) { (result, _ ) in
+            if let result = result {
+                thumbnail = result
+            }
+        }
+        return thumbnail
     }
 }
