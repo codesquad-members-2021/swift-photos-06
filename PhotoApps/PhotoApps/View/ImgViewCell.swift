029b2435a1dd8c8b39e9422ed25dbfb5ae6b707c
Song Lee <songlish233@gmail.com>
2021-03-26 11:00:36 +0900

    Create UIMenuItem

--- PhotoApps/PhotoApps/View/ImgViewCell.swift
+++ PhotoApps/PhotoApps/View/ImgViewCell.swift
@@ -11,9 +11,6 @@ import UIKit
 
 class ImgViewCell : UICollectionViewCell {
     
-    
-    
-    
     let bg : UIImageView = {
         let iv = UIImageView(frame: CGRect(x: 0, y: 0, width: 110, height: 50))
         return iv
@@ -24,15 +21,48 @@ class ImgViewCell : UICollectionViewCell {
         contentView.addSubview(bg)
 
         bg.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-//        bg.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
-//        bg.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
-//        bg.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-
-        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func enableSaveMenu() {
+        let save = UIMenuItem(title: "Save", action: #selector(longTouchedImgViewCell))
+        UIMenuController.shared.menuItems = [save]
+    }
+    
+    @objc func longTouchedImgViewCell(_ gesture: UILongPressGestureRecognizer) {
+        print("long touched!")
+    }
+    
 }
+
+//https://stackoverflow.com/questions/45243947/uilabel-long-press-gesture-for-menu-item-of-uimenucontroller
+//internal func handleLongPressed(_ gesture: UILongPressGestureRecognizer) {
+//        guard let gestureView = gesture.view, let superView = gestureView.superview else {
+//            return
+//        }
+//
+//        let menuController = UIMenuController.shared
+//
+//        guard !menuController.isMenuVisible, gestureView.canBecomeFirstResponder else {
+//            return
+//        }
+//
+//        gestureView.becomeFirstResponder()
+//
+//        menuController.menuItems = [
+//            UIMenuItem(
+//                title: "Custom Item",
+//                action: #selector(handleCustomAction(_:))
+//            ),
+//            UIMenuItem(
+//                title: "Copy",
+//                action: #selector(handleCopyAction(_:))
+//            )
+//        ]
+//
+//        menuController.setTargetRect(gestureView.frame, in: superView)
+//        menuController.setMenuVisible(true, animated: true)
+//    }
